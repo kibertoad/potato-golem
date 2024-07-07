ffe66e59ed6e12e0e074820fde39c07f8885f78b
Igor Savin <iselwin@gmail.com>
2024-07-07 22:00:35 +0300

    Add Phaser

--- games/shadow-agents/src/index.ts
+++ games/shadow-agents/src/index.ts
@@ -1,2 +1,23 @@
-// biome-ignore lint/suspicious/noConsoleLog: <explanation>
-console.log('hallo')
+import { Game } from 'phaser'
+
+const GameResolutions = {
+  default: {
+    width: 1280,
+    height: 720,
+  },
+}
+
+const resolution = GameResolutions.default
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: undefined,
+  width: resolution.width,
+  height: resolution.height,
+  plugins: {
+  },
+  scene: [],
+}
+
+// For some reason this must happen in root TS file
+new Game(config)
