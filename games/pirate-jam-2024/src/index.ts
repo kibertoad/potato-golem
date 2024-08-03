f7fad0424ba02e9b6ddb6b9b5b33df6d68f46a5c
Arturs Ziborovs <arturs.ziborovs@gmail.com>
2024-08-03 23:12:30 +0300

    Attempt to add FMOD

--- games/pirate-jam-2024/src/index.ts
+++ games/pirate-jam-2024/src/index.ts
@@ -1,11 +1,32 @@
 import Phaser, { Game } from 'phaser'
 import { instantiateContainer } from './model/diConfig'
 
+const fmodInit = require('../lib/fmodstudio')
+
 const GameResolutions = {
   default: {
     width: 2560,
     height: 1440,
   },
+}
+
+const fmod: FMOD = {
+	TOTAL_MEMORY: 24 * 1024 * 1024,
+	preRun: preRun,
+	onRuntimeInitialized : main
+};
+
+fmodInit(fmod); // Initialize the fmod object
+
+// === Callbacks ===
+function preRun() {
+	// not implemented yet
+  console.log('preRun')
+}
+
+function main() {
+	// not implemented yet
+  console.log('main')
 }
 
 const resolution = GameResolutions.default
