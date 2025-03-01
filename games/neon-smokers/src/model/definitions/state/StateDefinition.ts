23d6f81ec5c2e5f3fbe6a7847df5dcea9e896ac4
Igor Savin <iselwin@gmail.com>
2025-03-02 01:47:29 +0200

    Add states

---
+++ games/neon-smokers/src/model/definitions/state/StateDefinition.ts
@@ -0,0 +1,63 @@
+import type { ImageId } from '../../../registries/imageRegistry'
+import { RegistryEntityId } from '@potato-golem/core'
+import { ChoiceDefinition } from '../definitionInterfaces'
+import { choiceRegistry } from '../zones/01_district1/district1ChoiceDefinitions'
+
+export type StateGroup = 'physical' | 'emotional' | 'rumours' | 'knowledge' | 'assets' | 'reputation'
+
+export type StateDefinition = {
+  name: string
+  description: string
+  image: ImageId
+  group: StateGroup
+  maxAmount?: number // default is 100
+}
+
+export const stateRegistry = {
+  ENERGY: 'energy', // replenished by sleep and rest
+  EXHAUSTION: 'exhaustion', // replenished by enjoyable activities
+
+  /*
+  WOUNDS: 'wounds', // replenished by patching up
+  TRAUMAS: 'traumas', // replenished by deep treatment, slowly
+  STRESS: 'stress', // replenished by resting
+  PSYCHOSIS: 'psychosis', // replenished by therapy
+
+   */
+
+  EUROS: 'euros', // money
+
+  CRED: 'cred' // fame
+} as const
+
+export const stateDefinitions = {
+  energy: {
+    name: 'Energy',
+    description: 'Being able to act',
+    group: 'physical',
+    image: 'rocket',
+    maxAmount: 10
+  },
+  exhaustion: {
+    name: 'Exhaustion',
+    description: 'Being exhausted',
+    group: 'physical',
+    image: 'rocket',
+    maxAmount: 10,
+  },
+  euros: {
+    name: 'Euros',
+    description: 'Money',
+    group: 'assets',
+    image: 'rocket',
+    maxAmount: 10000000,
+  },
+  cred: {
+    name: 'Cred',
+    description: 'Renown, reputation, prestige - you name it',
+    group: 'reputation',
+    image: 'rocket',
+    maxAmount: 10000,
+  },
+
+} as const satisfies Record<RegistryEntityId<typeof stateRegistry>, StateDefinition>
--- games/neon-smokers/src/model/entities/WorldModel.ts
+++ games/neon-smokers/src/model/entities/WorldModel.ts
@@ -1,18 +1,27 @@
-import { removeFromArrayById } from '@potato-golem/core'
+import { LimitedNumber, RegistryEntityId, removeFromArrayById } from '@potato-golem/core'
 import  { ChoiceModel } from './narrative/ChoiceModel'
 import {ZoneBundle} from "../definitions/zones/common/ZoneBundle";
 import {LocationDefinition} from "../definitions/zones/common/LocationDefinition";
 import {district1Bundle} from "../definitions/zones/01_district1/district1Bundle";
+import { stateDefinitions, stateRegistry } from '../definitions/state/StateDefinition'
 
 export class WorldModel {
   public currentZone: ZoneBundle
   public currentLocation?: LocationDefinition
 
+  public readonly playerStates: Record<RegistryEntityId<typeof stateRegistry>, LimitedNumber>
   public readonly choices: ChoiceModel[] = []
-
 
   constructor() {
     this.currentZone = district1Bundle
+    // @ts-ignore
+    this.playerStates = {}
+    for (const stateId of Object.values(stateRegistry)) {
+      this.playerStates[stateId] = new LimitedNumber(0, stateDefinitions[stateId].maxAmount, false)
+    }
+
+    this.playerStates.energy.setValue(10)
+    this.playerStates.euros.setValue(1000)
   }
 
   addChoice(cardModel: ChoiceModel) {
